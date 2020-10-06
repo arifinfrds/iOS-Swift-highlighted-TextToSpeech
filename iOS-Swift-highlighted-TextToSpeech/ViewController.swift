1d4312d5067bf26f1c876eea6365f31191c6e171
Arifin Firdaus <arifin.firdaus1996@gmail.com>
2020-10-06 09:03:09 +0700

    Initial Commit

---
+++ iOS-Swift-highlighted-TextToSpeech/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS-Swift-highlighted-TextToSpeech
+//
+//  Created by Arifin Firdaus on 06/10/20.
+//  Copyright © 2020 Arifin Firdaus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
