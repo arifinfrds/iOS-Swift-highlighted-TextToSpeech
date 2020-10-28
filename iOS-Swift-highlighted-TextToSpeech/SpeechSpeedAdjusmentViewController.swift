93bed8e2e3a993746e5a7ae2318e88f0c28d3121
Arifin Firdaus <arifin.firdaus1996@gmail.com>
2020-10-28 11:52:47 +0700

    Change string mock for text to speech

--- iOS-Swift-highlighted-TextToSpeech/SpeechSpeedAdjusmentViewController.swift
+++ iOS-Swift-highlighted-TextToSpeech/SpeechSpeedAdjusmentViewController.swift
@@ -13,28 +13,29 @@ class SpeechSpeedAdjusmentViewController: UIViewController {
     @IBOutlet weak var textView: UITextView!
     var synthesizer: AVSpeechSynthesizer!
     var currentRange: NSRange = NSRange(location: 0, length: 0)
-    var string: String?
-    
+    var text: String?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        let generator = LoremIpsumGenerator()
+        
+        let generator = MockStringGenerator()
         textView.text = generator.generateText()
         
-        string = textView.text!
+        text = textView.text!
         
         synthesizer = AVSpeechSynthesizer()
         synthesizer.delegate = self
     }
     
     @IBAction func didTapSpeakButton(_ sender: UIBarButtonItem) {
-        synthesizer.speak(makeUtterance(for: AVSpeechUtteranceDefaultSpeechRate, with: string!))
+        synthesizer.speak(makeUtterance(for: AVSpeechUtteranceDefaultSpeechRate, with: text!))
     }
     
     @IBAction func didChangeSpeakSpeed(_ sender: UISlider) {
         if synthesizer.isSpeaking {
             synthesizer.stopSpeaking(at: .immediate)
         }
+        
         if isStillHaveTextThatNeedsToBeSpoken() {
             attemptSpeakTextThatNeedsToBeSpoken(withRate: sender.value)
         }
@@ -45,20 +46,23 @@ class SpeechSpeedAdjusmentViewController: UIViewController {
     }
     
     private func attemptSpeakTextThatNeedsToBeSpoken(withRate rate: Float) {
-        guard let string = string else { return }
-        if NSMaxRange(currentRange) >= string.count {
+        guard let text = self.text else { return }
+        if NSMaxRange(self.currentRange) >= text.count {
             return
         }
-        let startIndex = string.index(string.startIndex, offsetBy: NSMaxRange(currentRange))
+        let offfset = NSMaxRange(currentRange)
+        
+        let startIndex = text.index(text.startIndex, offsetBy: offfset)
         // let newString = string.substring(from: startIndex)
-        let newString = String(string[startIndex...])
-        self.string = newString
-        synthesizer.speak(makeUtterance(for: rate, with: string))
+        let newString = String(text[startIndex...])
+        self.text = newString
+        synthesizer.speak(makeUtterance(for: rate, with: text))
     }
     
-    func makeUtterance(for rate: Float, with str: String) -> AVSpeechUtterance {
+    private func makeUtterance(for rate: Float, with str: String) -> AVSpeechUtterance {
         let utterance = AVSpeechUtterance(string: str)
         utterance.rate = rate
+        utterance.volume = 0.2
         utterance.voice = AVSpeechSynthesisVoice(language: "en-US")
         return utterance
     }
@@ -71,18 +75,22 @@ class SpeechSpeedAdjusmentViewController: UIViewController {
 extension SpeechSpeedAdjusmentViewController: AVSpeechSynthesizerDelegate {
     
     func speechSynthesizer(_ synthesizer: AVSpeechSynthesizer, willSpeakRangeOfSpeechString characterRange: NSRange, utterance: AVSpeechUtterance) {
-        print("speaking")
         let mutableAttributedString = NSMutableAttributedString(string: utterance.speechString)
-        mutableAttributedString.addAttribute(.foregroundColor, value: UIColor.red, range: characterRange)
+        mutableAttributedString.addAttribute(
+            .foregroundColor,
+            value: UIColor.red,
+            range: characterRange
+        )
         textView.attributedText = mutableAttributedString
         
-        debugPrint(characterRange.toRange())
-        currentRange = characterRange
+        // debugPrint(characterRange.toRange())
+        self.currentRange = characterRange
     }
     
     func speechSynthesizer(_ synthesizer: AVSpeechSynthesizer, didFinish utterance: AVSpeechUtterance) {
-        print("finish")
         textView.attributedText = NSAttributedString(string: utterance.speechString)
+        // self.currentRange = NSRange(location: 0, length: 0)
+
     }
     
 }
